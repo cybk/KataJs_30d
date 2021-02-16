b0325e21535b1dc2c7a6f21d056b73e38a6ccd24
Juan Miguel Alcala Suarez <cybk2000@hotmail.com>
2021-02-15 21:52:18 -0600

    PArtial changes for some fun with webcam

--- 18_Webcam_Fun/scripts.js
+++ 18_Webcam_Fun/scripts.js
@@ -21,8 +21,11 @@ function paintCanvas () {
     canvas.width = width;
     canvas.height = height;
     
-    setInterval(() => {
+    return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        let pixels = ctx.getImageData(0, 0, width, height);
+        redEffect(pixels);
+        ctx.putImageData(pixels, 0, 0);
     }, 16)
 }
 
@@ -39,6 +42,14 @@ function takePhoto () {
     link.textContent = 'Download Image';
     link.innerHTML = `<img src="${data}" alt="Handsome Man"/>`;
     strip.insertBefore(link, strip.firstChild); 
+}
+
+function redEffect (pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] = pixels.data[i + 0] + 100;       // Red
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;    // Green
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;   // Blue
+    }
 }
 
 getVideo();
